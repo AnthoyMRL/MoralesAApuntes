7c1e0b84eaed7db9aa6f2311b1ac70a3a81f859a
AnthonyMRL <anthony.morales.velez@udla.edu.ec>
2025-06-15 18:03:59 -0500

    Se agrego la pagina para ver las notas

---
+++ MoralesAApuntes/Views/AllNotesPage.xaml.cs
@@ -0,0 +1,30 @@
+namespace MoralesAApuntes.Views;
+
+public partial class AllNotesPage : ContentPage
+{
+    public AllNotesPage()
+    {
+        InitializeComponent();
+        BindingContext = new Models.Allnotes();
+    }
+
+    protected override void OnAppearing()
+    {
+        ((Models.Allnotes)BindingContext).LoadNotes();
+    }
+
+    private async void Add_Clicked(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync(nameof(NotePage));
+    }
+
+    private async void notesCollection_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        if (e.CurrentSelection.Count != 0)
+        {
+            var note = (Models.Note)e.CurrentSelection[0];
+            await Shell.Current.GoToAsync($"{nameof(NotePage)}?{nameof(NotePage.ItemId)}={note.Filename}");
+            notesCollection.SelectedItem = null;
+        }
+    }
+}
