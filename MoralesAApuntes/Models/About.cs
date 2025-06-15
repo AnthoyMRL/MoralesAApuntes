86ad5ba77cc7fa25ff8c893eaa0cda1059abd5f9
AnthonyMRL <anthony.morales.velez@udla.edu.ec>
2025-06-15 17:51:56 -0500

    Se crearon los modelos para las notas

---
+++ MoralesAApuntes/Models/About.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoralesAApuntes.Models;
+    internal class About
+    {
+        public string Title => AppInfo.Name;
+        public string Version => AppInfo.VersionString;
+        public string MoreInfoUrl => "https://aka.ms/maui";
+        public string Name => "Anthony Moales";
+        public string Description => "Estudiante de Ingeniería de Software";
+        public string Hobbies => "Mis hobbies incluyen jugar videojuegos, escuchar musica, jugar futbol";
+    }
+
---
+++ MoralesAApuntes/Models/Allnotes.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoralesAApuntes.Models;
+    internal class Allnotes
+    {
+    public ObservableCollection<Note> Notes { get; set; } = new ObservableCollection<Note>();
+
+    public Allnotes() =>
+        LoadNotes();
+
+    public void LoadNotes()
+    {
+        Notes.Clear();
+
+        string appDataPath = FileSystem.AppDataDirectory;
+
+        IEnumerable<Note> notes = Directory
+            .EnumerateFiles(appDataPath, "*.notes.txt")
+            .Select(filename => new Note()
+            {
+                Filename = filename,
+                Text = File.ReadAllText(filename),
+                Date = File.GetLastWriteTime(filename)
+            })
+            .OrderByDescending(note => note.Date);
+
+        foreach (Note note in notes)
+            Notes.Add(note);
+    }
+}
